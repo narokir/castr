41dc5f0aed4e91310891441ed6425c173631d184
Narek <narek.kirakossian@gmail.com>
2020-08-23 14:50:56 -0600

    add action_text

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require('@rails/ujs').start();
 require('turbolinks').start();
 require('@rails/activestorage').start();
 require('channels');
+require('trix');
+require('@rails/actiontext');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -23,3 +25,6 @@ document.addEventListener('turbolinks:load', () => {
   });
   $('.toast').toast('show');
 });
+
+require('trix');
+require('@rails/actiontext');
